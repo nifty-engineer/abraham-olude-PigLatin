f958b3f308b8465ac896e47a69f9e654ec7e8474
Abraham Olude <abraham.olude.ao@gmail.com>
2023-10-31 04:19:40 +0000

    Complete Coding Challenge: Pig Latin

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,10 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+
+        String first = in.substring(0, 1);
+        String others = in.substring(1);
+
+        return others.concat(first) + "ay";
     }
 }
